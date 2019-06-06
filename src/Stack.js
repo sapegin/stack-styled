459b3226aa0dae70fe6f9d64c63452c4ab1ac571
Artem Sapegin <asapegin@wayfair.com>
2019-06-06 09:30:40 +0200

    Chore: Use new system() API from styled-system 5

--- src/Stack.js
+++ src/Stack.js
@@ -1,24 +1,10 @@
 import styled from 'styled-components';
-import { space, layout, flexbox, grid, style } from 'styled-system';
+import { space, layout, flexbox, grid, system } from 'styled-system';
 import propTypes, { propType } from '@styled-system/prop-types';
 
 const DEFAULT_SCALE = [0, 4, 8, 16, 32, 64, 128, 256, 512];
 
 const getPx = value => (typeof value === 'number' ? `${value}px` : value);
-
-const gap = style({
-	prop: 'gap',
-	cssProperty: 'gridGap',
-	key: 'space',
-	scale: DEFAULT_SCALE,
-});
-
-const minWidth = style({
-	prop: 'minWidth',
-	cssProperty: 'gridTemplateColumns',
-	transformValue: n =>
-		n ? `repeat(auto-fit, minmax(${getPx(n)}, 1fr))` : null,
-});
 
 /**
  * @visibleName Stack Styled
@@ -28,8 +14,17 @@ const Stack = styled('div')(
 		boxSizing: 'border-box',
 		display: 'grid',
 	},
-	gap,
-	minWidth,
+	system({
+		gap: {
+			property: 'gridGap',
+			scale: 'space',
+			defaultScale: DEFAULT_SCALE,
+		},
+		minWidth: {
+			property: 'gridTemplateColumns',
+			transform: n => (n ? `repeat(auto-fit, minmax(${getPx(n)}, 1fr))` : null),
+		},
+	}),
 	space,
 	layout,
 	flexbox,
